71d0c6a3a6d73f274868f28ba352e4d4ca10fb1c
Cemozerr <cemozer2018@u.northwestern.edu>
2018-08-24 17:56:06 +0300

    Read file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-    println!("{:?}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("Item prices:\n{}", contents);
 }
 
