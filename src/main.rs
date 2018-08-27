9b5ed93d75dc9daa764f7f31f83dbcde14d95f89
Cemozerr <cemozer2018@u.northwestern.edu>
2018-08-27 16:56:17 +0300

    Parse JSON

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+extern crate serde_json;
+
 use std::env;
 use std::fs;
+use serde_json::Value;
+
+/*
+struct Item{
+    name: String,
+    price: u16,
+}
+*/
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,6 +19,9 @@ fn main() {
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
 
-    println!("Item prices:\n{}", contents);
+    let json: Value = serde_json::from_str(&contents)
+        .expect("JSON was not well-formatted");
+
+     println!("{}", json);
 }
 
